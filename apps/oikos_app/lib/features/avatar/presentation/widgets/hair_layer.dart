e44b36f44d6067e0b95b719769be0d2c34a38e8d
Wanderson Rossini <wandersonrossini@gmail.com>
2026-07-18 14:16:03 +0200

    fix: corrigindo formato e posicionamento dos cabelos dos avatares para nao cobrirem os olhos

--- apps/oikos_app/lib/features/avatar/presentation/widgets/hair_layer.dart
+++ apps/oikos_app/lib/features/avatar/presentation/widgets/hair_layer.dart
@@ -13,7 +13,28 @@ class HairLayer extends StatelessWidget {
   Widget build(BuildContext context) {
     if (hairType == HairType.none) return const SizedBox.shrink();
     
-    final path = 'assets/avatars/hairs/hair_short01.svg';
+    String filename;
+    switch (hairType) {
+      case HairType.short01:
+        filename = 'hair_short01.svg';
+        break;
+      case HairType.short02:
+        filename = 'hair_short02.svg';
+        break;
+      case HairType.long01:
+        filename = 'hair_long01.svg';
+        break;
+      case HairType.long02:
+        filename = 'hair_long02.svg';
+        break;
+      case HairType.bun:
+        filename = 'hair_bun.svg';
+        break;
+      default:
+        filename = 'hair_short01.svg';
+    }
+    
+    final path = 'assets/avatars/hairs/$filename';
     return SvgPicture.asset(
       path,
       colorFilter: ColorFilter.mode(theme.hairColor, BlendMode.srcATop),
